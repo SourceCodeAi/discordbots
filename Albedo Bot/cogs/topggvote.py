8c9d4cde03b1a353459d53f3f07928c3f5b971a3
SourceCodeAi <69261605+SourceCodeAi@users.noreply.github.com>
2021-01-02 13:26:27 +0000

    Update topggvote.py

--- Albedo Bot/cogs/topggvote.py
+++ Albedo Bot/cogs/topggvote.py
@@ -8,7 +8,7 @@ class TopGG(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijc4OTk3NTM5NTIzODY3NDQ2MyIsImJvdCI6dHJ1ZSwiaWF0IjoxNjA4ODQ3NzI5fQ.SO7007Ty4UuHseSXJM4PfBLxZs8VZu77fEbmPWehxWY' # set this to your DBL token
+        self.token = 'top.gg token here' # set this to your DBL token
         self.dblpy = dbl.DBLClient(self.bot, self.token, autopost=True) # Autopost will post your guild count every 30 minutes
 
     @commands.Cog.listener()
